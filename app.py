12f7c721da926a74fae01b84832ba597fdeee91b
Joseph Samela <josephsamela@gmail.com>
2025-05-04 15:42:19 -0400

    create webapp

---
+++ app.py
@@ -0,0 +1,181 @@
+from flask import Flask, render_template, request, url_for, redirect, make_response, jsonify
+
+import datetime
+
+from db import Database
+db = Database('db.xlsx')
+
+# Initialize Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def wallet():
+    user = authentication_check(request)
+    if not user:
+        return redirect(url_for("login"))
+    return render_template(
+        "wallet.html", 
+        user=user
+    )
+
+@app.route("/user/<username>")
+def user(username):
+    user = db.get_user(username)
+    return render_template(
+        "user.html", 
+        user=user
+    )
+
+# Login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    match request.method:
+        case 'GET':
+            return render_template("login.html")
+        case 'POST':
+            username = request.form.get("username")
+            password = request.form.get("password")
+            user = db.get_user(username)
+            
+            if not user or user.password != password:
+                return render_template("login.html", error="Invalid username or password")
+            session = db.start_session(username)
+
+            response = make_response(redirect(url_for('wallet')))
+            response.set_cookie('session', session.token, expires=session.expires)
+            return response
+
+@app.route("/logout")
+def logout():
+    user = authentication_check(request)
+    db.end_session(user.username)
+    return redirect(url_for("wallet"))
+
+@app.route("/transaction", methods=["POST"])
+def transaction():
+
+    user_from = authentication_check(request)
+    if not user_from:
+        return redirect(url_for("login"))
+
+    transaction_type = request.form.get("transaction_type")
+    user_to = request.form.get("to")
+
+    match transaction_type:
+
+        # This route supports two types of transactions:
+        #   1. "lbc" = Send an amount of lbc to a recipient.
+        #   2. "nft" = Send a nft to a recipient.
+
+        case 'lbc':
+            transaction_amount = request.form.get("amount")
+
+            # Validation Checks
+
+            if not int(transaction_amount):
+                return render_template("send_lbc.html", user=user_from, error="Can only send whole number of LBC.")
+
+            if '.' in transaction_amount:
+                return render_template("send_lbc.html", user=user_from, error="Can only send whole number of LBC.")
+
+            if not db.get_user(user_to):
+                return render_template("send_lbc.html", user=user_from, error="Recipient does not exist.")
+
+            if int(transaction_amount) > user_from.balance:
+                return render_template("send_lbc.html", user=user_from, error="Insufficient balance")
+
+            db.write_transaction(
+                user_from=user_from.id,
+                user_to=db.get_user(user_to).id,
+                amount=int(transaction_amount)
+            )
+
+            return make_response(redirect(url_for('success')))
+
+        case 'nft':
+
+            transaction_token = request.form.get("token")
+
+            if not db.get_user(user_to):
+                return render_template("send_nft.html", user=user_from, error="Recipient does not exist.")
+
+            if not int(transaction_token):
+                return render_template("send_nft.html", user=user_from, error="You can only send Tokens you own.")
+
+            if not int(transaction_token) in user_from.tokens:
+                return render_template("send_nft.html", user=user_from, error="You can only send Tokens you own.")
+
+            db.write_transaction(
+                user_from=user_from.id,
+                user_to=db.get_user(user_to).id,
+                token=transaction_token
+            )
+
+            return make_response(redirect(url_for('success')))
+
+        case _:
+            return redirect(url_for("wallet"))
+
+@app.route("/send_lbc")
+def send_lbc():
+    user = authentication_check(request)
+    if not user:
+        return redirect(url_for("login"))
+
+    return render_template("send_lbc.html", user=user, users=db.user_list(user))
+
+@app.route("/send_nft")
+def send_nft():
+    user = authentication_check(request)
+    if not user:
+        return redirect(url_for("login"))
+
+    return render_template("send_nft.html", user=user, users=db.user_list(user))
+
+@app.route("/success")
+def success():
+    user = authentication_check(request)
+    if not user:
+        return redirect(url_for("login"))
+
+    return render_template(
+        "success.html", 
+        user=user
+    )
+
+def authentication_check(request):
+    # Check if browser session exists.
+    session = request.cookies.get('session')
+    # Check if user exists.
+    user = db.check_session(session)
+    return user
+
+@app.template_filter()
+def format_date(d):
+    dt = datetime.datetime.fromisoformat(d)
+    return dt.strftime("%b %-d, %Y %-I:%M %p")
+
+@app.template_filter()
+def format_credit_date(d):
+    dt = datetime.datetime.fromisoformat(d)
+    return dt.strftime("%m/%Y")
+
+@app.template_filter()
+def format_account_number(d):
+    
+    account_id = ''
+
+    r = 16 - len(str(d))
+    d = '0'*r + str(d)
+
+    c = 0
+    for i in d:
+        account_id += i
+        c += 1
+        if c == 4:
+            account_id += ' '
+            c = 0
+    return account_id
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5001)
---
+++ db.py
@@ -0,0 +1,251 @@
+import openpyxl
+import datetime
+import uuid
+import json
+
+class Database:
+    def __init__(self, path):
+        self.path = path
+        self.workbook = openpyxl.load_workbook(path)
+        self.sessions = {}
+
+        self.load_db()
+
+    def load_db(self):
+        self.users = self.load_data(self.workbook, 'users', User)
+        self.tokens = self.load_data(self.workbook, 'tokens', Token)
+        self.transactions = self.load_data(self.workbook, 'transactions', Transaction)
+
+    def write_transaction(self, user_from, user_to, amount=0, token=None ):
+        worksheet=self.workbook['transactions']
+
+        if token:
+            worksheet.append([
+                max(self.transactions.keys())+1,
+                datetime.datetime.now().isoformat(),
+                user_from,
+                user_to,
+                None,
+                int(token)
+            ])
+
+        else:
+            worksheet.append([
+                max(self.transactions.keys())+1,
+                datetime.datetime.now().isoformat(),
+                user_from,
+                user_to,
+                int(amount),
+                None
+            ])
+
+        self.workbook.save(self.path)
+        self.load_db()
+
+    def load_data(self, workbook, worksheet, pattern):
+        first = True
+        headers = None
+        data = {}
+        for i, row in enumerate(workbook[worksheet].iter_rows(values_only=True)):
+            if first:
+                headers = row
+                first = False
+            else:
+                rowdict = dict()
+                for i, header in enumerate(headers):
+                    rowdict[header] = row[i]
+                obj = pattern(rowdict, self)
+                data[obj.id] = obj
+        return data
+        
+    def get_user(self, username):
+        for id,user in self.users.items():
+            if user.username == username:
+                return user
+        return None
+
+    def start_session(self, username):
+        session = Session(username)
+        self.sessions[username] = session
+        return session
+
+    def end_session(self, username):
+        self.sessions.pop(username)
+
+    def check_session(self, token):
+        for id,session in self.sessions.items():
+            if session.token == token:
+                return self.get_user(session.username)
+        return None
+
+    def user_list(self, user):
+        users = []
+        for id,u in self.users.items():
+            if u.username == 'system':
+                continue
+            if u.username == user.username:
+                continue
+            users.append(u.username)
+        return users
+
+    def generate_new_id(self):
+        from random import randint
+        s = ''
+        for i in range(16):
+            s = s + str(randint(0,9))
+        return s
+
+class Object:
+    def __init__(self, d, db):
+        self.db = db
+        for key in d:
+            setattr(self, key, d[key])
+
+    def __iter__(self):
+        for p in dir(self):
+            if not p.startswith('__'):
+                yield p, getattr(self, p)
+
+    def __repr__(self):
+        d = {}
+        for p in dir(self):
+            if p.startswith('__'):
+                continue
+            if p == 'db':
+                continue
+
+            if type(getattr(self, p)) == User:
+                d[p] = dict(getattr(self, p))
+            else:
+                d[p] = getattr(self, p)
+
+        return json.dumps(d)
+
+def safe_serialize(obj):
+  default = lambda o: f"<<non-serializable: {type(o).__qualname__}>>"
+  return json.dumps(obj, default=default)
+
+class User(Object):
+
+    @property
+    def tokens(self):
+        tokens = {}
+        for id, transaction in self.db.transactions.items():
+
+            if transaction.amount:
+                continue
+
+            if getattr(transaction, 'to') is self.id:
+                tokens[transaction.token] = self.db.tokens[transaction.token].to_dict()
+
+            if getattr(transaction, 'from') is self.id:
+                tokens.pop(transaction.token)
+
+        return tokens
+
+    @property
+    def balance(self):
+        balance = 0
+
+        for id, transaction in self.db.transactions.items():
+
+            if not transaction.amount:
+                continue
+
+            if getattr(transaction, 'to') is self.id:
+                balance += transaction.amount
+
+            if getattr(transaction, 'from') is self.id:
+                balance -= transaction.amount
+        
+        return balance
+    
+    @property
+    def transactions(self):
+        transactions = []
+
+        for id, transaction in self.db.transactions.items():
+
+            if getattr(transaction, 'to') is self.id:
+                transactions.append(transaction.to_dict())
+
+            if getattr(transaction, 'from') is self.id:
+                transactions.append(transaction.to_dict())
+
+        transactions.reverse()
+
+        return transactions
+
+    def to_dict(self):
+        d = {
+            'id': self.id,
+            'username': self.username,
+            'nickname': self.nickname,
+            'created_at': self.created_at,
+            'balance': self.balance,
+            'transactions': self.transactions,
+            'tokens': self.tokens
+        }
+        return d
+
+class Token(Object):
+
+    @property
+    def owner(self):
+        owner = None
+        for id, transaction in self.db.transactions.items():
+
+            if transaction.amount:
+                continue
+
+            if transaction.token is self.id:
+                owner = self.db.users[transaction.to]
+
+        return owner
+
+    def to_dict(self):
+        d = {
+            'id': self.id,
+            'created_at': self.created_at,
+            'note': self.note,
+            'url': self.url
+        }
+        return d
+
+class Transaction(Object):
+
+    @property
+    def user_from(self):
+        return self.db.users[getattr(self, 'from')]
+
+    @property
+    def user_to(self):
+        return self.db.users[getattr(self, 'to')]
+    
+    def to_dict(self):
+        d = {
+            'id': self.id,
+            'timestamp': self.timestamp,
+            'amount': self.amount,
+            'token': self.token,
+            'user_from_id': self.user_from.id,
+            'user_from_username': self.user_from.username,
+            'user_from_nickname': self.user_from.nickname,
+            'user_to_id': self.user_to.id,
+            'user_to_username': self.user_to.username,
+            'user_to_nickname': self.user_to.nickname,
+            'token_url': None,
+            'token_note': None
+
+        }
+        if self.token:
+            d['token_url'] = self.db.tokens[self.token].url
+            d['token_note'] = self.db.tokens[self.token].note
+
+        return d
+
+class Session:
+    def __init__(self, username):
+        self.username = username
+        self.expires = datetime.datetime.now() + datetime.timedelta(days=30)
+        self.token = str(uuid.uuid4())
